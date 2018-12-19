e2838f35f8f8aec846eb464b0a9e225154d6217e
LehensGivris <louis1996.lelann@gmail.com>
2018-12-19 17:00:51 +0100

    Update createBase.sql

--- createBase.sql
+++ createBase.sql
@@ -1,22 +1,7 @@
 SET datestyle TO 'european';
 
-DROP TABLE IF EXISTS Photographgie, Discriminant, Date, Remarque, Support, NegatifOuReversible, Taille, Photographgie_Lieu, Lambert93, Fichier_Photographie, Fichier, Iconographie_Photographie, Iconographie, Sujet_Photographie, Sujet, Personne_Photographie, Personne, Personne_Metier, Metier;
-
-CREATE TABLE Photographgie(
-	id SERIAL,
-	cindoc VARCHAR(255),
-	serie VARCHAR(255),
-	article INTEGER,
-	discriminant VARCHAR(255),
-	description VARCHAR(65535),
-	notes VARCHAR(65535),
-	date INTEGER,
-	RemarqueidRemarque INTEGER,
-	PRIMARY KEY(id),
-	FOREIGN KEY (discriminant) REFERENCES Discriminant(discriminant),
-	FOREIGN KEY (date) REFERENCES Date(id),
-	FOREIGN KEY (RemarqueidRemarque) REFERENCES Remarque(idRemarque)
-);
+DROP TABLE IF EXISTS Photographgie_Lieu, Fichier_Photographie, Iconographie_Photographie, Sujet_Photographie, Personne_Photographie, Personne_Metier;
+DROP TABLE IF EXISTS Discriminant, Date, Remarque, Photographgie, NegatifOuReversible, Taille, Support, Lambert93, Fichier, Iconographie, Sujet, Personne, Metier;
 
 CREATE TABLE Discriminant(
 	discriminant VARCHAR(255),
@@ -35,6 +20,32 @@ CREATE TABLE Remarque(
 	remarque VARCHAR(255)
 );
 
+CREATE TABLE Photographgie(
+	id SERIAL,
+	cindoc VARCHAR(255),
+	serie VARCHAR(255),
+	article INTEGER,
+	discriminant VARCHAR(255),
+	description VARCHAR(65535),
+	notes VARCHAR(65535),
+	date INTEGER,
+	RemarqueidRemarque INTEGER,
+	PRIMARY KEY(id),
+	FOREIGN KEY (discriminant) REFERENCES Discriminant(discriminant),
+	FOREIGN KEY (date) REFERENCES Date(id),
+	FOREIGN KEY (RemarqueidRemarque) REFERENCES Remarque(idRemarque)
+);
+
+CREATE TABLE NegatifOuReversible(
+	nr VARCHAR(255),
+	PRIMARY KEY(nr)
+);
+
+CREATE TABLE Taille(
+	taille VARCHAR(255),
+	PRIMARY KEY(taille)
+);
+
 CREATE TABLE Support(
 	id SERIAL,
 	idphoto INTEGER,
@@ -48,24 +59,6 @@ CREATE TABLE Support(
 	FOREIGN KEY (nr) REFERENCES NegatifOuReversible(nr)
 );
 
-CREATE TABLE NegatifOuReversible(
-	nr VARCHAR(255),
-	PRIMARY KEY(nr)
-);
-
-CREATE TABLE Taille(
-	taille VARCHAR(255),
-	PRIMARY KEY(taille)
-);
-
-CREATE TABLE Photographgie_Lieu(
-	Photographgieid INTEGER,
-	LambertId INTEGER,
-	PRIMARY KEY(Photographgieid,LambertId),
-	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id),
-	FOREIGN KEY (LambertId) REFERENCES Lambert93(refLieux)
-);
-
 CREATE TABLE Lambert93(
 	refLieux INTEGER,
 	codeInsee INTEGER,
@@ -76,26 +69,10 @@ CREATE TABLE Lambert93(
 	PRIMARY KEY(refLieux)
 );
 
-CREATE TABLE Fichier_Photographie(
-	FichieridFichier INTEGER,
-	Photographgieid INTEGER,
-	PRIMARY KEY(FichieridFichier,Photographgieid),
-	FOREIGN KEY (FichieridFichier) REFERENCES Fichier(idFichier),
-	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id)
-);
-
 CREATE TABLE Fichier(
 	idFichier SERIAL,
 	NomFichier VARCHAR(255),
 	PRIMARY KEY(idFichier)
-);
-
-CREATE TABLE Iconographie_Photographie(
-	IconographieIndex VARCHAR(255),
-	Photographgieid INTEGER,
-	PRIMARY KEY(IconographieIndex,Photographgieid),
-	FOREIGN KEY (IconographieIndex) REFERENCES Iconographie(Index),
-	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id)
 );
 
 CREATE TABLE Iconographie(
@@ -103,25 +80,9 @@ CREATE TABLE Iconographie(
 	PRIMARY KEY(Index)
 );
 
-CREATE TABLE Sujet_Photographie(
-	SujetIdsujet INTEGER,
-	Photographgieid INTEGER,
-	PRIMARY KEY(SujetIdsujet,Photographgieid),
-	FOREIGN KEY (SujetIdsujet) REFERENCES Sujet(idSujet),
-	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id)
-);
-
 CREATE TABLE Sujet(
 	idSujet INTEGER,
 	sujet VARCHAR(65535)
-);
-
-CREATE TABLE Personne_Photographie(
-	Personnepersonne VARCHAR(255),
-	Photographgieid INTEGER,
-	PRIMARY KEY(Personnepersonne,Photographgieid),
-	FOREIGN KEY (Personnepersonne) REFERENCES Personne(personne),
-	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id)
 );
 
 CREATE TABLE Personne(
@@ -133,16 +94,56 @@ CREATE TABLE Personne(
 	PRIMARY KEY(personne)
 );
 
+CREATE TABLE Metier(
+	idMetier INTEGER,
+	Designation VARCHAR(255),
+	PRIMARY KEY(idMetier)
+);
+
+CREATE TABLE Photographgie_Lieu(
+	Photographgieid INTEGER,
+	LambertId INTEGER,
+	PRIMARY KEY(Photographgieid,LambertId),
+	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id),
+	FOREIGN KEY (LambertId) REFERENCES Lambert93(refLieux)
+);
+
+CREATE TABLE Fichier_Photographie(
+	FichieridFichier INTEGER,
+	Photographgieid INTEGER,
+	PRIMARY KEY(FichieridFichier,Photographgieid),
+	FOREIGN KEY (FichieridFichier) REFERENCES Fichier(idFichier),
+	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id)
+);
+
+CREATE TABLE Iconographie_Photographie(
+	IconographieIndex VARCHAR(255),
+	Photographgieid INTEGER,
+	PRIMARY KEY(IconographieIndex,Photographgieid),
+	FOREIGN KEY (IconographieIndex) REFERENCES Iconographie(Index),
+	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id)
+);
+
+CREATE TABLE Sujet_Photographie(
+	SujetIdsujet INTEGER,
+	Photographgieid INTEGER,
+	PRIMARY KEY(SujetIdsujet,Photographgieid),
+	FOREIGN KEY (SujetIdsujet) REFERENCES Sujet(idSujet),
+	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id)
+);
+
+CREATE TABLE Personne_Photographie(
+	Personnepersonne VARCHAR(255),
+	Photographgieid INTEGER,
+	PRIMARY KEY(Personnepersonne,Photographgieid),
+	FOREIGN KEY (Personnepersonne) REFERENCES Personne(personne),
+	FOREIGN KEY (Photographgieid) REFERENCES Photographgie(id)
+);
+
 CREATE TABLE Personne_Metier(
 	Personnepersonne VARCHAR(255),
 	MetieridMetier INTEGER,
 	PRIMARY KEY(Personnepersonne,MetieridMetier),
 	FOREIGN KEY (Personnepersonne) REFERENCES Personne(personne),
 	FOREIGN KEY (MetieridMetier) REFERENCES Metier(idMetier)
-);
-
-CREATE TABLE Metier(
-	idMetier INTEGER,
-	Designation VARCHAR(255),
-	PRIMARY KEY(idMetier)
 );
