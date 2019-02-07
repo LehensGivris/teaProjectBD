664d6c21c55d3eac174b3228920e0d90af94a7c1
LehensGivris <louis1996.lelann@gmail.com>
2019-02-07 15:17:08 +0100

    Correction fonctions

--- Part_01/donnees.sql
+++ Part_01/donnees.sql
@@ -667,7 +667,7 @@ CREATE OR REPLACE FUNCTION verifMetier()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF EXISTS (SELECT * FROM Metier WHERE Designation = New.Designation);
+        IF EXISTS (SELECT * FROM Metier WHERE Designation = New.Designation)
         THEN
             RETURN NEW;
         ELSE
@@ -680,7 +680,7 @@ CREATE OR REPLACE FUNCTION verifPers()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF NOT EXISTS (SELECT * FROM Personne WHERE Nom = New.Nom AND Prenom = New.Prenom AND Representation = New.Representation AND Notes = New.Notes);
+        IF NOT EXISTS (SELECT * FROM Personne WHERE Nom = New.Nom AND Prenom = New.Prenom AND Representation = New.Representation AND Notes = New.Notes)
         THEN
             RETURN NEW;
         ELSE
@@ -693,7 +693,7 @@ CREATE OR REPLACE FUNCTION verifDate()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF EXISTS (SELECT * FROM Date WHERE jour = New.jour AND jour_bis = New.jour_bis AND mois = New.mois AND mois_bis = New.mois_bis AND annee = New.annee AND annee_bis = New.annee_bis);
+        IF EXISTS (SELECT * FROM Date WHERE jour = New.jour AND jour_bis = New.jour_bis AND mois = New.mois AND mois_bis = New.mois_bis AND annee = New.annee AND annee_bis = New.annee_bis)
         THEN
             RETURN NEW;
         ELSE
@@ -706,7 +706,7 @@ CREATE OR REPLACE FUNCTION verifLamb()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF EXISTS (SELECT * FROM Lambert93 WHERE nom = New.nom);
+        IF EXISTS (SELECT * FROM Lambert93 WHERE nom = New.nom)
         THEN
             RETURN NEW;
         ELSE
@@ -719,7 +719,7 @@ CREATE OR REPLACE FUNCTION verifSujet()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF NOT EXISTS (SELECT * FROM Sujet WHERE Sujet = New.Sujet);
+        IF NOT EXISTS (SELECT * FROM Sujet WHERE Sujet = New.Sujet)
         THEN
             RETURN NEW;
         ELSE
@@ -732,7 +732,7 @@ CREATE OR REPLACE FUNCTION verifFichier()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF NOT EXISTS (SELECT * FROM Fichier WHERE NomFichier = New.NomFichier);
+        IF NOT EXISTS (SELECT * FROM Fichier WHERE NomFichier = New.NomFichier)
         THEN
             RETURN NEW;
         ELSE
--- Part_01/function.sql
+++ Part_01/function.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION verifMetier()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF EXISTS (SELECT * FROM Metier WHERE Designation = New.Designation);
+        IF EXISTS (SELECT * FROM Metier WHERE Designation = New.Designation)
         THEN
             RETURN NEW;
         ELSE
@@ -15,7 +15,7 @@ CREATE OR REPLACE FUNCTION verifPers()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF NOT EXISTS (SELECT * FROM Personne WHERE Nom = New.Nom AND Prenom = New.Prenom AND Representation = New.Representation AND Notes = New.Notes);
+        IF NOT EXISTS (SELECT * FROM Personne WHERE Nom = New.Nom AND Prenom = New.Prenom AND Representation = New.Representation AND Notes = New.Notes)
         THEN
             RETURN NEW;
         ELSE
@@ -28,7 +28,7 @@ CREATE OR REPLACE FUNCTION verifDate()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF EXISTS (SELECT * FROM Date WHERE jour = New.jour AND jour_bis = New.jour_bis AND mois = New.mois AND mois_bis = New.mois_bis AND annee = New.annee AND annee_bis = New.annee_bis);
+        IF EXISTS (SELECT * FROM Date WHERE jour = New.jour AND jour_bis = New.jour_bis AND mois = New.mois AND mois_bis = New.mois_bis AND annee = New.annee AND annee_bis = New.annee_bis)
         THEN
             RETURN NEW;
         ELSE
@@ -41,7 +41,7 @@ CREATE OR REPLACE FUNCTION verifLamb()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF EXISTS (SELECT * FROM Lambert93 WHERE nom = New.nom);
+        IF EXISTS (SELECT * FROM Lambert93 WHERE nom = New.nom)
         THEN
             RETURN NEW;
         ELSE
@@ -54,7 +54,7 @@ CREATE OR REPLACE FUNCTION verifSujet()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF NOT EXISTS (SELECT * FROM Sujet WHERE Sujet = New.Sujet);
+        IF NOT EXISTS (SELECT * FROM Sujet WHERE Sujet = New.Sujet)
         THEN
             RETURN NEW;
         ELSE
@@ -67,7 +67,7 @@ CREATE OR REPLACE FUNCTION verifFichier()
 RETURNS TRIGGER AS $$
     DECLARE
     BEGIN
-        IF NOT EXISTS (SELECT * FROM Fichier WHERE NomFichier = New.NomFichier);
+        IF NOT EXISTS (SELECT * FROM Fichier WHERE NomFichier = New.NomFichier)
         THEN
             RETURN NEW;
         ELSE
