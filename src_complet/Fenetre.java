dd565fdffbf7f58901bd9ef762f9c27d3c08ebb4
LehensGivris <40198675+LehensGivris@users.noreply.github.com>
2019-02-17 23:06:39 +0100

    Revert "dsqq"
    This reverts commit 5a8e91d8b918329b418e1d3780bb5c313fcf856f.

--- src_complet/Fenetre.java
+++ src_complet/Fenetre.java
@@ -372,8 +372,8 @@ public class Fenetre extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) throws Exception{
-        DBComunicator bdd = new DBComunicator();
+    public static void main(String args[]) {
+        bdd = new DBComunicator();
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
